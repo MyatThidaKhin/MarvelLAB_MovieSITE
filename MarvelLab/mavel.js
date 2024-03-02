6550dc90f590db0af71d1ab22cedeee9c52bacf2
MyatThidaKhin <118741872+MyatThidaKhin@users.noreply.github.com>
2024-03-02 16:04:07 +0630

    Add files via upload

---
+++ MarvelLab/mavel.js
@@ -0,0 +1,184 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll',() =>{
+    header.classList.toggle('shadow',window.scrollY > 0);
+});
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+var swiper = new Swiper(".marvel", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 55500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+var swiper = new Swiper(".series", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 55500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+var swiper = new Swiper(".upcoming", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 55500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+var swiper = new Swiper(".avengers", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 55500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+//show video
+
+let palyBotton = document.querySelector(".watch-btn");
+let video = document.querySelector(".video-container");
+let myvideo = document.querySelector("#myvideo");
+let closebtn = document.querySelector(".close-video");
+
+palyBotton.onclick = () =>{
+    video.classList.add("show-video");
+    //Auto play
+    myvideo.play();
+}
+closebtn.onclick = () =>{
+    video.classList.remove("show-video");
+    //Auto play
+    myvideo.pause();
+}
